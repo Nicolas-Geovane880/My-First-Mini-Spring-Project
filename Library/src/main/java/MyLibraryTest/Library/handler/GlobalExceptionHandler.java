c7d3a6fb775f35db71cc1f93f87900a8d8cd7c02
Nicolas <devnicolasmateus@gmail.com>
2025-11-01 14:34:21 -0300

    C2 - Changing GlobalHandler to handle with more exceptions

--- Library/src/main/java/MyLibraryTest/Library/handler/GlobalExceptionHandler.java
+++ Library/src/main/java/MyLibraryTest/Library/handler/GlobalExceptionHandler.java
@@ -1,22 +1,24 @@
 package MyLibraryTest.Library.handler;
 
 import MyLibraryTest.Library.exceptions.*;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import jakarta.annotation.Nullable;
+import org.springframework.http.*;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @ControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler (BookNotFoundException.class)
-    public ResponseEntity<RequestExceptionDetails> bookNotFoundExceptionHandler 
+    public ResponseEntity<RequestExceptionDetails> handleBookNotFoundException
             (BookNotFoundException e) {
 
         return new ResponseEntity<> (RequestExceptionDetails.builder()
@@ -28,7 +30,7 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler (InvalidRequestValuesException.class)
-    public ResponseEntity<RequestExceptionDetails> invalidRequestValuesExceptionHandler 
+    public ResponseEntity<RequestExceptionDetails> handleInvalidRequestValuesException
             (InvalidRequestValuesException e) {
 
         return new ResponseEntity<> (RequestExceptionDetails.builder()
@@ -38,10 +40,10 @@ public class GlobalExceptionHandler {
                 .timestamp(LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd/MM/yyyy - HH:mm:ss")))
                 .build(), HttpStatus.BAD_REQUEST);
     }
-    
-    @ExceptionHandler (MethodArgumentNotValidException.class)
-    public ResponseEntity<FieldsValidationExceptionDetails> methodArgumentNotValidExceptionHandler
-            (MethodArgumentNotValidException e) {
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid
+            (MethodArgumentNotValidException e, @Nullable HttpHeaders headers, @Nullable HttpStatusCode status, @Nullable WebRequest request) {
 
         List<FieldError> fieldErrors = e.getFieldErrors();
 
@@ -61,6 +63,20 @@ public class GlobalExceptionHandler {
                 .fields(fields)
                 .fieldsMessage(fieldsMessage)
                 .build(), HttpStatus.BAD_REQUEST);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleExceptionInternal
+            (Exception e, @Nullable Object body, @Nullable HttpHeaders headers, HttpStatusCode status, @Nullable WebRequest request) {
+
+        ExceptionDetails build = ExceptionDetails.builder()
+                .status(status.value())
+                .title(e.getMessage())
+                .message("An error occur in the JSON format.")
+                .timestamp(LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd/MM/yyyy - HH:mm:ss")))
+                .build();
+
+        return new ResponseEntity<>(build, headers, status);
     }
 }
 
--- Readme.md
+++ Readme.md
@@ -1,33 +1,6 @@
-# My first Spring Boot Project
+### Notable Changes
 
-Hi, I have been studying java for a significant time, and now is the moment to diving deep into Spring Boot.
+The GlobalHandlerException was changed to support HttpsInternalsExceptions, like malformed JSON body.
 
-This project is a simple CRUD, simulating a library, where we can create, find, remove/delete or update some books.
+GlobalHandlerException now extends ResponseEntityExceptionHandler, and by now it can handle with more exceptions.
 
-### Technologies used to code this project:
-- (including some dependencies of the Spring boot like: Spring Data, Spring Web, Spring Test, Spring DevTools, Spring Validation)
-
-
-- Lombok
-
-
-- MapStruct
-
-
-- H2 database (based on memory), doesn't need to integrate a real database, but just run the application
-
-
-- Postman (recommended to make easier all requests)
-
-Note: Using H2 database in memory, all the data will be lost if the application stops.
-If you want to keep all data even with the application not running, use the H2 based on files, or integrate a real database, create a schema and pass your credentials to application.properties / application.yml.
-
-### Features
-
-- CRUD operations: create, read, update or delete books
-
-- Filtering: search books by author, price or initial letter of the title;
-
----
-
-When cloning this project, make sure to load all dependencies properly (by reloading all maven projects and typing "mvn clean install" in the terminal).
